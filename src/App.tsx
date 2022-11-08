b0976df65ebd642035fd96f216bf45018f29d055
Agrawal_ Jatin (jatagrawal@deloitte.com) <jatagrawal@your.hostname.com>
2022-11-08 16:43:59 +0530

    graphql-app

---
+++ src/App.tsx
@@ -0,0 +1,93 @@
+import './App.css';
+import React, { useState } from "react";
+import { GraphQLEditor, PassedSchema, GraphQLGqlEditor } from "graphql-editor";
+
+const schemas = {
+  pizza: `
+    type Query{
+      pizzas: [Pizza!]
+    }
+    `,
+  pizzaLibrary: `
+    type Pizza{
+      name:String
+    }
+    `,
+  };
+
+function App() {
+
+  let fileReader:any;
+
+  const fileRead = (e:any) => {
+    const content = fileReader.result;
+    console.log(content);
+    setMySchema({
+      code: content,
+      libraries: "",
+    });
+  };
+
+  const fileChosen = (file:any) => {
+    fileReader = new FileReader();
+    fileReader.onloadend = fileRead;
+    fileReader.readAsText(file.target.files[0]);
+  };
+
+  // const [gql, setGql] = useState("");
+  const [mySchema, setMySchema] = useState<PassedSchema>({
+    code: schemas.pizza,
+    libraries: schemas.pizzaLibrary,
+  });
+
+  const newGraph = () => {
+    setMySchema({
+      code: '',
+      libraries: ''
+    });
+  };
+
+  return (
+    <>
+      <div className="main">
+        <div className="sideNav">
+          <button className="button" onClick={() => newGraph()}>
+            Create New Graph
+          </button>
+          <br></br>
+          <label htmlFor='file-input' className='label'>Load Existing</label>
+          <input
+            type="file"
+            id="file-input"
+            accept=".graphqls"
+            onChange={(e) => fileChosen(e)}
+          />
+        </div>
+        <div
+          style={{
+            flex: 1,
+            width: "100%",
+            height: "100%",
+            alignSelf: "stretch",
+            display: "flex",
+            position: "relative",
+          }}
+        >
+          <GraphQLEditor
+            setSchema={(props: any) => setMySchema(props)}
+            schema={mySchema}
+          />
+
+          {/* <GraphQLGqlEditor
+            gql={gql}
+            setGql={(gqlString) => setGql(gqlString)}
+            schema={{ code: mySchema.code }}
+          /> */}
+
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default App;
